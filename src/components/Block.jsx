2440509a06d47f5da4d1d7556694cfabe384a8f4
FloatingRiver8 <maceiramariana@gmail.com>
2023-06-27 17:37:54 -0300

    feat(block and home) animation on clock number and card

--- src/components/Block.jsx
+++ src/components/Block.jsx
@@ -4,7 +4,8 @@ import { Alchemy, Network } from "alchemy-sdk";
 import { useState } from 'react';
 import { Outlet, useParams } from 'react-router-dom'
 import { NavLink } from 'react-router-dom/cjs/react-router-dom.min';
-import BlockInfo from '../Pages/BlockInfo';
+import { useRef } from 'react';
+import 'animate.css';
 
 
 const settings = {
@@ -14,42 +15,54 @@ const settings = {
 
 const alchemy = new Alchemy(settings);
 
-const Block = ({ blockNumber, getBlockNumber, setShowSearchResult}) => {
-
+const Block = ({ blockNumber, getBlockNumber, setShowSearchResult }) => {
+  const numberDiv = document.querySelector('.numberDiv')
   const handleOnLastBlock = () => {
     getBlockNumber()
     setShowSearchResult(false)
+
+
+    const buttonElement = buttonRef.current;
+    buttonElement.classList.remove('animate__animated',  'animate__fadeInDown');
+    buttonElement.style.setProperty('--animate-duration', '1.5s');
+    void buttonElement.offsetWidth; // Reinicia la animación
+  
+    buttonElement.classList.add('animate__animated', 'animate__fadeInDown' );
+    buttonElement.style.setProperty('--animate-duration', '1.5s');
+
     
-  }
-  return (
+}
+const buttonRef = useRef(null);
+
+return (
 
 
-    <div class="flex justify-center flex-col flex-wrap place-items-center space-y-2 ">
+  <div class="flex justify-center flex-col flex-wrap place-items-center space-y-2 ">
 
-      <div class="font-light text-4xl tracking-tight  hover:font-bold hover:text-green-500  hover:tracking-widest"  style={{ textShadow: "-1px -1px cyan" }}>
-        <NavLink to={`block/${blockNumber}`}> <p>#{blockNumber} </p></NavLink>
-      </div>
-     
-      <div class=' bg-yellow-100 '>
-        <button onClick={handleOnLastBlock}
-          class="group relative inline-block focus:outline-none focus:ring"
-        >
-          <span
-            class="absolute inset-0 translate-x-0 translate-y-0 bg-yellow-300 transition-transform group-hover:translate-y-1.5 group-hover:translate-x-1.5"
-          ></span>
-          <span
-            class="relative inline-block border-2 border-current px-8 py-3 text-sm font-bold uppercase tracking-widest "style={{ textShadow: "-1px -1px blue" }}
-          >
-            Get last block
-          </span>
-        </button>
-      </div>
-
-
-
-      
+    <div ref={buttonRef} class="font-light text-4xl tracking-tight  hover:font-bold  hover:tracking-widest " style={{ textShadow: "-1px -1px cyan" }}>
+      <NavLink to={`block/${blockNumber}`}> <p>#{blockNumber} </p></NavLink>
     </div>
-  )
+
+    <div class=' bg-yellow-100 '>
+      <button onClick={handleOnLastBlock}
+        class="group relative inline-block focus:outline-none focus:ring"
+      >
+        <span
+          class="absolute inset-0 translate-x-0 translate-y-0 bg-yellow-300 transition-transform group-hover:translate-y-1.5 group-hover:translate-x-1.5"
+        ></span>
+        <span
+          class="relative inline-block border-2 border-current px-8 py-3 text-sm font-bold uppercase tracking-widest " style={{ textShadow: "-1px -1px blue" }}
+        >
+          Get last block
+        </span>
+      </button>
+    </div>
+
+
+
+
+  </div>
+)
 }
 
 export default Block
