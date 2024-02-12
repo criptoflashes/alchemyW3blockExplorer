f75fadb6a7a0c96ed9d84d2890da17a39d3a264f
FloatingRiver8 <maceiramariana@gmail.com>
2024-02-12 14:15:14 -0800

    add git secret to fix api call

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -14,6 +14,8 @@ import { NavLink } from 'react-router-dom';
 // Refer to the README doc for more information about using API
 // keys in client-side code. You should never do this in production
 // level code.
+
+
 const settings = {
     apiKey: process.env.REACT_APP_ALCHEMY_API_KEY,
     network: Network.ETH_MAINNET,
